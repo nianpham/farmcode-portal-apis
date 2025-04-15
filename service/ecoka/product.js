bad83732eac85813afbb289a8eee962723a0f4ee
nguyenconghieudata <hieunc@farmcode.io.vn>
2025-04-15 22:36:24 +0700

    update: ecoka product show statú

--- service/ecoka/product.js
+++ service/ecoka/product.js
@@ -1,10 +1,9 @@
 const { ecokaModel } = require('~/model');
-const { ObjectId } = require("mongodb");
+const { ObjectId } = require('mongodb');
 
 async function getAllProducts() {
   const products = await ecokaModel.product.find({});
-    return products
-      .filter(product => !product.deleted_at);
+  return products.filter((product) => !product.deleted_at);
 }
 
 async function getProductById(id) {
@@ -12,7 +11,10 @@ async function getProductById(id) {
 }
 
 async function updateProduct(id, data) {
-  return ecokaModel.product.updateOne({ _id: new ObjectId(id) }, data);
+  return ecokaModel.product.updateOne(
+    { _id: new ObjectId(id) },
+    data
+  );
 }
 
 async function createProduct(data) {
@@ -27,7 +29,8 @@ async function createProduct(data) {
     japan_description: data.japan_description,
     category: data.category,
     price: data.price,
-  }
+    show_status: data.show_status,
+  };
   return await ecokaModel.product.insertOne(insert_product);
 }
 
@@ -35,7 +38,10 @@ async function deleteProduct(id) {
   const dataUpdate = {
     deleted_at: new Date(),
   };
-  return ecokaModel.product.updateOne({ _id: new ObjectId(id) }, dataUpdate);
+  return ecokaModel.product.updateOne(
+    { _id: new ObjectId(id) },
+    dataUpdate
+  );
 }
 
 module.exports = {
@@ -43,5 +49,5 @@ module.exports = {
   getProductById,
   createProduct,
   updateProduct,
-  deleteProduct
+  deleteProduct,
 };
